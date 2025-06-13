46eb4dae1d3b121b130b91c9c9e04d9f3091ee88
nakajimayoshi <Yoshi@hdsimulations.com>
2025-06-13 17:06:34 +0900

    rewrite

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,301 @@
+use crate::LoadBalancerError;
+use clap::error;
+use core::time;
+use geo::Coord;
+use log;
+use log::error;
+use serde::Deserialize;
+use std::fs;
+use std::io;
+use std::net::Ipv4Addr;
+use std::net::{AddrParseError, IpAddr};
+use std::str::FromStr;
+use tokio::net::TcpSocket;
+use tokio::net::TcpStream;
+use tokio::time::timeout;
+use toml;
+use url::Url;
 
+const VALID_VERSION_STRS: [&str; 1] = ["1"];
+
+const LOG_FILE_SIZE_HARD_LIMIT_MB: usize = 10;
+
+#[derive(Debug, thiserror::Error)]
+pub enum JalbConfigError {
+    #[error("could not open config file")]
+    FileIOError(#[from] io::Error),
+    #[error("failed to deserialize from config file")]
+    DeserializationError(#[from] toml::de::Error),
+    #[error("the provided address was neither a valid url or socket address {0}")]
+    InvalidNetworkTarget(String),
+    #[error("unknown load balancer strategy specified {0}")]
+    UnknownLoadBalancerStrategy(String),
+    #[error("unknown jalb config version specified {0}. Valid versions are {1}")]
+    UnknownConfigVersion(String, String),
+}
+
+#[derive(Debug, Deserialize)]
+enum JalbConfigVersion {
+    #[serde(rename = "1")]
+    V1,
+}
+
+impl Default for JalbConfigVersion {
+    fn default() -> Self {
+        JalbConfigVersion::V1
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq, Clone, Copy)]
+pub enum LoadBalancerType {
+    #[serde(rename = "application")]
+    Application,
+    #[serde(rename = "network")]
+    Network,
+}
+
+#[derive(Debug, Deserialize, Clone, Copy)]
+pub enum LoadBalancerStrategy {
+    #[serde(rename = "round_robin")]
+    RoundRobin,
+    #[serde(rename = "least_used")]
+    LeastUsed,
+    #[serde(rename = "weighted_average")]
+    WeightedAverage,
+    #[serde(rename = "geo")]
+    Geolocation,
+}
+
+#[derive(Debug, Deserialize)]
+struct LoadBalancerConfig {
+    #[serde(rename = "type")]
+    load_balancer_type: LoadBalancerType,
+    strategy: LoadBalancerStrategy,
+    listener_address: Option<IpAddr>,
+    port: Option<u16>,
+    max_connections: u32,
+    max_requests_per_connection: u32,
+}
+
+#[derive(Debug, Deserialize)]
+struct SecurityConfig {
+    ip_whitelist: Vec<String>,
+    ip_blacklist: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BackendOptions {
+    pub name: String,
+    pub health_endpoint: Option<String>,
+    pub health_check_interval_seconds: Option<u32>,
+    pub health_check_timeout_seconds: Option<u32>,
+    pub failed_request_threshold: Option<u32>,
+    pub request_timeout_seconds: Option<u32>,
+    pub rate_limit: Option<u64>,
+    pub nodes: Vec<Node>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+enum NetworkTarget {
+    Url(url::Url),
+    SocketAddr(std::net::SocketAddr),
+}
+
+impl NetworkTarget {
+    fn as_string(&self) -> String {
+        match self {
+            Self::SocketAddr(addr) => addr.to_string(),
+            Self::Url(url) => url.to_string(),
+        }
+    }
+}
+
+impl FromStr for NetworkTarget {
+    type Err = JalbConfigError;
+    fn from_str(s: &str) -> Result<Self, JalbConfigError> {
+        type Err = JalbConfigError;
+
+        if let Ok(url) = Url::parse(s) {
+            return Ok(NetworkTarget::Url(url));
+        }
+
+        if let Ok(socket_addr) = s.parse::<std::net::SocketAddr>() {
+            return Ok(Self::SocketAddr(socket_addr));
+        }
+
+        Err(JalbConfigError::InvalidNetworkTarget(s.to_string()))
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Node {
+    address: NetworkTarget,
+    weight: Option<u32>,
+    coordinates: Option<[f32; 2]>,
+}
+
+fn tcpsocket_from_address(addr: &std::net::SocketAddr) -> Result<TcpSocket, io::Error> {
+    if addr.is_ipv4() {
+        return TcpSocket::new_v4();
+    }
+
+    return TcpSocket::new_v6();
+}
+
+impl Node {
+    async fn get_health_tcp(&self, connect_timeout: time::Duration) -> Result<bool, io::Error> {
+        match self.address {
+            NetworkTarget::SocketAddr(socket_addr) => {
+                let socket = tcpsocket_from_address(&socket_addr)?;
+
+                let future = socket.connect(socket_addr);
+                match timeout(connect_timeout, future).await {
+                    Ok(Ok(stream)) => return Ok(true),
+                    Ok(Err(e)) => {
+                        error!("health check for {} failed: {}", socket_addr, e);
+                        return Err(e);
+                    }
+                    Err(_) => {
+                        error!(
+                            "tcp health check for {} timed out after {:?}",
+                            socket_addr, connect_timeout
+                        );
+                        return Ok(false);
+                    }
+                }
+            }
+            NetworkTarget::Url(ref url) => {
+                let future = TcpStream::connect(url.to_string());
+                match timeout(connect_timeout, future).await {
+                    Ok(Ok(stream)) => return Ok(true),
+                    Ok(Err(e)) => {
+                        error!("health check for {} failed: {}", url.as_str(), e);
+                        return Err(e);
+                    }
+                    Err(_) => {
+                        error!(
+                            "tcp health check for {} timed out after {:?}",
+                            url.as_str(),
+                            connect_timeout
+                        );
+                        return Ok(false);
+                    }
+                }
+            }
+        }
+
+        Ok(false)
+    }
+}
+
+impl Node {
+    pub fn get_addr(&self) -> NetworkTarget {
+        self.address.clone()
+    }
+
+    pub fn get_weight(&self) -> Option<u32> {
+        self.weight
+    }
+
+    pub fn get_coordinates(&self) -> Option<Coord<f32>> {
+        if let Some(coordinates) = self.coordinates {
+            return Some(Coord {
+                x: coordinates[0],
+                y: coordinates[1],
+            });
+        }
+
+        None
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct LoggingConfig {
+    log_level: Option<log::Level>,
+    rotate_logs: bool,
+    log_capacity_mb: Option<usize>,
+    path: String,
+}
+
+impl Default for LoggingConfig {
+    fn default() -> Self {
+        let default_logfile_path = "./jalb_log.txt";
+
+        println!("DEFAULT INVOKED!");
+        Self {
+            log_level: Some(log::Level::Error),
+            log_capacity_mb: Some(LOG_FILE_SIZE_HARD_LIMIT_MB * 1024 * 1024),
+            rotate_logs: true,
+            path: default_logfile_path.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct JalbConfig {
+    loadbalancer: LoadBalancerConfig,
+    #[serde(default)]
+    logging: LoggingConfig,
+    security: SecurityConfig,
+    backends: Vec<BackendOptions>,
+}
+
+impl JalbConfig {
+    pub fn load_from_config_file(path: &str) -> Result<JalbConfig, JalbConfigError> {
+        let toml_str = fs::read_to_string(path)?;
+
+        let mut config = toml::from_str::<JalbConfig>(&toml_str)?;
+
+        println!("{:#?}", config);
+
+        Ok(config)
+    }
+
+    pub fn strategy(&self) -> LoadBalancerStrategy {
+        self.loadbalancer.strategy
+    }
+
+    pub fn load_balancer_type(&self) -> LoadBalancerType {
+        self.loadbalancer.load_balancer_type
+    }
+
+    pub fn listener_address(&self) -> IpAddr {
+        self.loadbalancer
+            .listener_address
+            .unwrap_or(IpAddr::from_str("127.0.0.1").unwrap())
+    }
+
+    pub fn rotate_logs(&self) -> bool {
+        self.logging.rotate_logs
+    }
+
+    pub fn log_file_max_size(&self) -> usize {
+        const BYTES_PER_MEGABYTE: usize = 1024 * 1024;
+
+        if let Some(max_size) = self.logging.log_capacity_mb {
+            println!("max size req: {}", max_size);
+            return max_size * BYTES_PER_MEGABYTE;
+        }
+
+        LOG_FILE_SIZE_HARD_LIMIT_MB * BYTES_PER_MEGABYTE
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_should_load_from_file() -> Result<(), JalbConfigError> {
+        let config = JalbConfig::load_from_config_file("jalb.toml")?;
+        config.load_balancer_type();
+        assert!(config.rotate_logs() == true);
+        assert!(config.log_file_max_size() == 10485760);
+        let ip = config.listener_address();
+        assert!(ip.is_ipv4());
+        assert!(ip.to_string() == "127.0.0.1");
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use std::{fmt::Debug, io, net::IpAddr, sync::Arc};
+use std::{collections::HashSet, fmt::Debug, io, net::{IpAddr, ToSocketAddrs}, path::Path, sync::Arc};
 
+use serde::Deserialize;
 use tokio::{
     self,
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream},
 };
 
-use backend::Backend;
-use clap::{self, Parser, builder::Str};
-use isocountry::CountryCode;
-use selectors::{RoundRobinSelector, Selector};
+use config::{JalbConfig, LoadBalancerStrategy};
+
+use clap::{self, builder::Str, error, Parser};
 
 use log::{error, info};
+use url::Url;
 
-mod backend;
+use crate::config::{BackendOptions, JalbConfigError, Node};
+
 mod config;
-mod selectors;
-mod tests;
 
 // make a load balancer with the following requirements:
 // 1. Multi-strategy (e.g. Round Robin, Least Connections, Weighted Round Robin, Geo-based, etc.)
@@ -25,207 +25,101 @@ mod tests;
 // 5. FFFFFFFAST ZOOOM
 // 6. Built-in monitoring & analytics
 
-enum Region {
-    Asia(CountryCode),
-    Africa(CountryCode),
-    Europe(CountryCode),
-    Oceania(CountryCode),
-    NorthAmerica(CountryCode),
-    SouthAmerica(CountryCode),
-    Unknown,
-}
-
 struct Source {
     ip_addr: IpAddr,
-    region: Option<Region>,
 }
 
 impl Source {
     pub fn new(ip_addr: IpAddr) -> Source {
         Self {
             ip_addr,
-            region: None,
         }
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Strategy {
-    RoundRobin,
-    LeastConnections,
-    WeightedRoundRobin,
-    Geospatial,
+struct LoadBalancerBuilder {
+    // backends: Vec<Backend>,
+    // selector: dyn Selector,
 }
 
-#[derive(Debug, Clone)]
-struct LoadBalancer<T>
-where
-    T: Clone + std::fmt::Debug + Send,
-    T: selectors::Selector,
-{
-    services: Vec<Backend>,
-    whitelist: Option<Vec<IpAddr>>,
-    blacklist: Option<Vec<IpAddr>>,
-    selector: T,
-    session_requests: u64,
+impl LoadBalancerBuilder {
+    
 }
 
-impl<T> LoadBalancer<T>
-where
-    T: Clone + std::fmt::Debug + Send + Default,
-    T: Selector,
-{
-    pub fn new() -> LoadBalancer<T> {
+#[derive(Debug, thiserror::Error)]
+enum LoadBalancerError {
+    #[error("failed to connect to backend")]
+    IOError(#[from] io::Error),
+    #[error("failed to open tcp socket")]
+    SocketOpenError(String)
+}
+
+
+
+pub trait LoadBalancer {
+    fn select_addr(&mut self) -> Option<&url::Url>;
+    fn add_backend(&mut self, config: &BackendOptions) -> Result<(), LoadBalancerError>;
+}
+
+pub struct NetworkLoadBalancerOptions {
+    ip_blacklist: Option<Vec<IpAddr>>,
+    ip_whitelist: Option<Vec<IpAddr>>,
+}
+
+
+
+pub struct Backend {
+    options: BackendOptions
+}
+
+impl Backend {
+    pub fn new(options: BackendOptions) -> Self {
+        Self { options: options }
+    }
+
+
+}
+
+
+#[derive(Debug)]
+struct RoundRobin {
+    last_idx: usize,
+    pool: HashSet<url::Url> 
+}
+
+impl Default for RoundRobin {
+    fn default() -> Self {
         Self {
-            services: vec![],
-            whitelist: None,
-            blacklist: None,
-            selector: T::default(),
-            session_requests: 0,
+            last_idx: 0,
+            pool: HashSet::new(),
         }
-    }
-
-    pub fn with_backends(services: Vec<Backend>) -> LoadBalancer<T> {
-        Self {
-            services,
-            whitelist: None,
-            blacklist: None,
-            selector: T::default(),
-            session_requests: 0,
-        }
-    }
-
-    fn increment_session_requests(&mut self) {
-        self.session_requests += 1
-    }
-
-    pub fn get_session_requests(&self) -> u64 {
-        self.session_requests
-    }
-
-    fn select_backend(&mut self) -> usize {
-        self.selector.select_service(&self.services)
-    }
-
-    pub async fn serve_request(
-        &mut self,
-        client_stream: &mut tokio::net::TcpStream,
-    ) -> Result<(), io::Error> {
-        if let Ok(addr) = client_stream.peer_addr() {
-            let client_ip = addr.ip();
-
-            if let Some(blacklist) = &self.blacklist {
-                if blacklist.contains(&client_ip) {
-                    return Err(io::Error::new(
-                        io::ErrorKind::PermissionDenied,
-                        "IP is blacklisted",
-                    ));
-                }
-            }
-
-            // Check if whitelist is enabled and IP is not in whitelist
-            if let Some(whitelist) = &self.whitelist {
-                if !whitelist.is_empty() && !whitelist.contains(&client_ip) {
-                    return Err(io::Error::new(
-                        io::ErrorKind::PermissionDenied,
-                        "IP is not in whitelist",
-                    ));
-                }
-            }
-        }
-
-        let backend_idx = self.select_backend();
-        let backend = self
-            .services
-            .get(backend_idx)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "No backend available"))?;
-
-        let mut server_stream = tokio::net::TcpStream::connect(&backend.uri.to_string()).await?;
-
-        // Set reasonable timeouts
-        // client_stream.set_read_timeout(Some(Duration::from_secs(10)))?;
-        // client_stream.set_write_timeout(Some(Duration::from_secs(10)))?;
-        // server_stream.set_read_timeout(Some(Duration::from_secs(10)))?;
-        // server_stream.set_write_timeout(Some(Duration::from_secs(10)))?;
-
-        // Buffer for data transfer
-        let mut buffer = [0; 8192]; // 8KB buffer
-
-        let mut client_closed = false;
-        let mut server_closed = false;
-
-        // Proxy loop
-        while !client_closed && !server_closed {
-            // Client -> Server
-            if !client_closed {
-                match client_stream.read(&mut buffer).await {
-                    Ok(0) => {
-                        // Client closed connection
-                        client_closed = true;
-                        server_stream.shutdown().await?;
-                    }
-                    Ok(n) => {
-                        // Forward data to server
-                        match server_stream.write_all(&buffer[0..n]).await {
-                            Ok(_) => {
-                                // Successful write to server
-                                self.increment_session_requests();
-                            }
-                            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                                continue;
-                            }
-                            Err(e) => return Err(e),
-                        }
-                    }
-                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                        // No data available, continue
-                    }
-                    Err(e) => return Err(e),
-                }
-            }
-
-            // Server -> Client
-            if !server_closed {
-                match server_stream.read(&mut buffer).await {
-                    Ok(0) => {
-                        // Server closed connection
-                        server_closed = true;
-                        client_stream.shutdown().await?;
-                    }
-                    Ok(n) => {
-                        // Forward data to client
-                        match client_stream.write_all(&buffer[0..n]).await {
-                            Ok(_) => {
-                                // Successful write to client
-                            }
-                            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                                // Would block, try again later
-                                continue;
-                            }
-                            Err(e) => return Err(e),
-                        }
-                    }
-                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                        // No data available, continue
-                    }
-                    Err(e) => return Err(e),
-                }
-            }
-
-            // Small sleep to avoid CPU spin
-            // std::thread::sleep(Duration::from_millis(1));
-        }
-
-        Ok(())
     }
 }
 
-#[derive(Debug, Clone)]
-enum LogLevel {
-    Error,
-    Warning,
-    Debug,
+impl LoadBalancer for RoundRobin {
+    fn select_addr(&mut self) -> Option<&url::Url> {
+        if self.pool.is_empty() {
+            return None;
+        }
+
+        return Some(url::Url)
+    }
+
+    fn add_backend(&mut self, config: &BackendOptions) -> Result<(), JalbConfigError> {
+       
+       
+        self.pool.insert()
+    }
 }
+
+
+struct NetworkLoadBalancer<T: LoadBalancer> {
+    inner: T
+}
+
+impl<T: LoadBalancer> NetworkLoadBalancer<T> {
+}
+
 
 #[derive(clap::Parser)]
 #[command(version, about, long_about = None)]
@@ -240,39 +134,8 @@ struct Args {
     worker_threads: usize, // log_level: LogLevel
 }
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
-async fn main() -> Result<(), io::Error> {
-    let args = Args::parse();
-    let listener =
-        tokio::net::TcpListener::bind(format!("{}:{}", args.listener_addr, args.port)).await?;
 
-    let listener_addr = listener.local_addr()?;
-
-    info!("Jalb balancer is listening on {:?}", listener_addr);
-
-    let backends = vec![
-        Backend::new("127.0.0.1:3001").unwrap(),
-        Backend::new("127.0.0.1:3002").unwrap(),
-        Backend::new("127.0.0.1:3003").unwrap(),
-        Backend::new("127.0.0.1:3004").unwrap(),
-        Backend::new("127.0.0.1:3005").unwrap(),
-        Backend::new("127.0.0.1:3006").unwrap(),
-        Backend::new("127.0.0.1:3007").unwrap(),
-        Backend::new("127.0.0.1:3008").unwrap(),
-        Backend::new("127.0.0.1:3009").unwrap(),
-        Backend::new("127.0.0.1:3010").unwrap(),
-    ];
-
-    let balancer = LoadBalancer::<RoundRobinSelector>::with_backends(backends);
-    let balancer = Arc::new(tokio::sync::Mutex::new(balancer));
-
-    while let Ok((mut stream, _addr)) = listener.accept().await {
-        let balancer_clone = Arc::clone(&balancer);
-        tokio::spawn(async move {
-            let mut balancer = balancer_clone.lock().await;
-            balancer.serve_request(&mut stream).await.unwrap();
-        });
-    }
-
-    Ok(())
+#[tokio::main]
+async fn main() {
+    println!("hello world");
 }
